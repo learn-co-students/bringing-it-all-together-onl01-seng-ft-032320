7ef1fea273b2cbcd32768fd1eb5ed49cb8d8774f
wingerwingman <justinwinger4@gmail.com>
2020-04-22 13:32:20 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,52 @@
+class Dog 
+    attr_accessor :name, :breed 
+    attr_reader :id
+    
+    def initialize(id: nil, name:, breed:)
+        @id = id
+        @name = name 
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+            )
+            SQL
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        DB[:conn].execute('DROP TABLE dogs')
+    end
+
+    def save
+        sql = <<-SQL
+        INSERT INTO dogs (name, breed) 
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql,self.name,self.breed)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+        self
+    end
+
+    def self.create(name:, breed:)
+        # binding.pry
+        dog_attributes = {name: name,breed: breed}
+        dog = Dog.new(dog_attributes)
+        dog.save
+    end
+
+    def self.new_from_db(row)
+      attributes = {
+        :id = row[0]
+        :name = row[1]
+        :breed = row[2]
+      }
+      self.new(attributes)
+    end
+
+end
